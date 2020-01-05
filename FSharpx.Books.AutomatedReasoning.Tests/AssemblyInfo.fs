686cccd2e1d99c30680b4b806aaa9a4879c74c41
sky <sky7drake@gmail.com>
2020-01-04 22:22:01 -0800

    support VS16 (2019) - add .fsproj and .sln

---
+++ FSharpx.Books.AutomatedReasoning.Tests/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace FSharpx.Books.AutomatedReasoning.Tests.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("FSharpx.Books.AutomatedReasoning.Tests")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("FSharpx.Books.AutomatedReasoning.Tests")>]
+[<assembly: AssemblyCopyright("Copyright ©  2020")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("e9f1fa3e-2e87-45b3-96ce-2bb55417e781")>]
+
+// Version information for an assembly consists of the following four values:
+//
+//       Major Version
+//       Minor Version
+//       Build Number
+//       Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
